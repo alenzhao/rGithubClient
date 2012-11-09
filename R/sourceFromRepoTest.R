b0c0b93b56e4d1bd8b81ab9a75ed417231029192
Christopher Bare <chris.bare@sagebase.org>
2012-11-09 09:27:36 -0800

    Try out another implementation of sourceFromRepo

---
+++ R/sourceFromRepoTest.R
@@ -0,0 +1,25 @@
+
+## usage:
+## repo <- getRepo("/user/MyProject")
+## sfr(repo, "my/special/script.R")
+## # objects in global environment
+## e <- new.env()
+## sfr(repo, "my/special/script.R", local=e)
+## # objects in environment e
+sfr <- function(repository, repositoryPath, ...) {
+  
+  ## MAKE SURE repositoryPath EXISTS
+  if( !all(repositoryPath %in% repository@tree$path) ){
+    stop("repositoryPath does not exists within this repository - cannot source")
+  }
+  
+  ## GRAB THE sha VALUES FROM THESE PATHS AND BUILD URLS FOR THEIR BLOBS
+  theseShas <- repository@tree$sha[match(repositoryPath, repository@tree$path)]
+  constructedURLs <- paste("https://api.github.com/repos", repository@user, repository@repo, "git/blobs", theseShas, sep="/")
+  
+  # note: is there a way to get RCurl to return a "file-like" connection object
+  for (url in constructedURLs) {
+    txt <- getURL(url, .opts=list(httpHeader = c(Accept="application/vnd.github.raw"), low.speed.time=60, low.speed.limit=1, connecttimeout=300, followlocation=TRUE, ssl.verifypeer=TRUE, verbose = FALSE))
+    source(file=textConnection(txt), ...)
+  }
+}
